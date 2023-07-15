7273dc3c0ac796c5361477f081fffc59095c3d71
MinSu_Kim <MinSu_Kim@outlook.kr>
2023-07-15 22:26:28 +0900

    simple SocketIO example

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import { Server } from "socket.io";
 import express from "express";
 
 const app = express();
@@ -15,37 +15,11 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // HTTP 서버, WebSocket 서버 둘 다 실행
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  console.log("Connected to the Browser!!!");
-
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-
-  socket.on("message", (data) => {
-    let msg = data.toString();
-
-    const parseMsg = JSON.parse(msg);
-
-    switch (parseMsg.type) {
-      case "new_message":
-        sockets.forEach((s) => {
-          s.send(`${socket.nickname}: ${parseMsg.payload}`);
-        });
-        break;
-      case "nickname":
-        socket["nickname"] = parseMsg.payload;
-        break;
-    }
-  });
-
-  socket.on("close", () => {
-    console.log("Disconnected from the Browser");
-  });
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
