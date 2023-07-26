d1dc4d7693f7d7fe8818de4ce00569ecf59dbec8
김민수 <mskim.spring@gmail.com>
2023-07-26 10:35:14 +0900

    Change Video Camera

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -95,6 +95,14 @@ function handleCameraClick() {
 async function handleCameraChange() {
   // console.log(camerasSelect.value);
   await getMedia(camerasSelect.value);
+
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection.getSenders().find(sender => {
+      sender.track.kind === "video"
+    });
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
