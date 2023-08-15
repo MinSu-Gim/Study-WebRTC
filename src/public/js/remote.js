f6409282c297182b5dfec9bbe11ed0036b4e8033
MinSu <mskim.spring@gmail.com>
2023-08-15 23:47:12 +0900

    v0.1

--- src/public/js/remote.js
+++ src/public/js/remote.js
@@ -1,113 +1,11 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
-const muteBtn = document.getElementById("mute");
-const cameraBtn = document.getElementById("camera");
-const camerasSelect = document.getElementById("cameras");
-
 const enterBtn = document.getElementById("call");
 
 enterBtn.hidden = true;
 
 let myStream;
-let muted = false;
-let cameraOff = false;
-
-async function getCameras() {
-  try {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    // console.log(devices);
-    const cameras = devices.filter((devcie) => devcie.kind === "videoinput");
-    // console.log(cameras);
-    const currentCamera = myStream.getVideoTracks()[0];
-
-    cameras.forEach((camera) => {
-      const option = document.createElement("option");
-      option.value = camera.deviceId;
-      option.innerText = camera.label;
-
-      if (currentCamera.label === camera.label) {
-        option.selected = true;
-      }
-      camerasSelect.appendChild(option);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-async function getMedia(deviceId) {
-  const initialConstrains = {
-    audio: true,
-    video: { facingMode: "user" },
-  };
-
-  const cameraConstraints = {
-    audio: true,
-    video: { deviceId: { exact: deviceId } },
-  };
-
-  try {
-    myStream = await navigator.mediaDevices.getUserMedia(deviceId ? cameraConstraints : initialConstrains);
-    share_screen.srcObject = myStream;
-
-    if (!deviceId) {
-      await getCameras();
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-// getMedia();
-
-function handleMuteClick() {
-  // console.log(myStream.getAudioTracks());
-  // console.log(myStream.getVideoTracks());
-
-  myStream.getAudioTracks().forEach((track) => {
-    track.enabled = !track.enabled;
-  });
-
-  if (!muted) {
-    muteBtn.innerText = "UnMute";
-    muted = true;
-  } else {
-    muteBtn.innerText = "Mute";
-    muted = false;
-  }
-}
-
-function handleCameraClick() {
-  myStream.getVideoTracks().forEach((track) => {
-    track.enabled = !track.enabled;
-  });
-
-  if (cameraOff) {
-    cameraBtn.innerText = "Turn Camera Off";
-    cameraOff = false;
-  } else {
-    cameraBtn.innerText = "Turn Camera On";
-    cameraOff = true;
-  }
-}
-
-async function handleCameraChange() {
-  // console.log(camerasSelect.value);
-  await getMedia(camerasSelect.value);
-
-  if (myPeerConnection) {
-    const videoTrack = myStream.getVideoTracks()[0];
-    const videoSender = myPeerConnection.getSenders().find((sender) => {
-      sender.track.kind === "video";
-    });
-    videoSender.replaceTrack(videoTrack);
-  }
-}
-
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
-camerasSelect.addEventListener("input", handleCameraChange);
 
 /**
  * Welcome Form (Join a Room!)
@@ -122,7 +20,7 @@ async function initCall() {
   welcome.hidden = true;
   enterBtn.hidden = false;
 
-  await getMedia();
+  myStream = new MediaStream();
 
   makeConnection();
 }
@@ -197,8 +95,6 @@ socket.on("close-event", () => {
 
 window.onload = function () {
   var video = document.querySelector("#myFace");
-  var stream11 = document.querySelector("#myStream");
-
   /**
    * Mouse, Keyboard Event
    */
@@ -256,7 +152,7 @@ function handleAddStream(data) {
   console.log("Mine Stream:", myStream);
 
   // const peerFace = document.getElementById("peerFace");
-  share_screen.srcObject = data.stream;
+  myFace.srcObject = data.stream;
 }
 
 let myPeerConnection;
@@ -279,81 +175,3 @@ function makeConnection() {
 
   myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
 }
-
-// async function startCapture() {
-//   let captureStream;
-
-//   try {
-//     captureStream = await navigator.mediaDevices.getDisplayMedia(
-//       {
-//         video: { cursor: 'always' },
-//         audio: { echoCancellation: true, noiseSuppression: true },
-//       }
-//     );
-
-//     const videoTrack = myStream.getVideoTracks()[0];
-
-//     console.log("b:", myFace)
-//     myFace.srcObject = captureStream; // 내 비디오 공유 화면으로 변경
-//     console.log("a:", myFace)
-//     if (myPeerConnection) {
-
-//       const videoSender = captureStream.getSenders();
-
-//       let result;
-//       for (let i=0; i < videoSender.length; i++) {
-//         if (videoSender[i].track.kind === "video") {
-//           console.log("vs:", videoSender[i]);
-//           result = videoSender[i];
-//           break;
-//         }
-//       }
-
-//       result.replaceTrack(videoTrack);
-
-//       // console.log("videTrack:", videoTrack);
-//       // console.log("videoSender:", videoSender);
-
-//       // videoSender.replaceTrack(videoTrack);
-//     }
-
-//   } catch (err) {
-//     console.error(`Error: ${err}`);
-//   }
-//   return captureStream;
-// }
-
-const startCapture = () => {
-  navigator.mediaDevices
-    .getDisplayMedia({
-      video: { cursor: "always" },
-      audio: { echoCancellation: true, noiseSuppression: true },
-    })
-    .then((stream) => {
-      share_screen.srcObject = stream; // 내 비디오 공유 화면으로 변경
-      const videoTrack = stream.getVideoTracks()[0];
-      myPeerConnection
-        .getSenders()
-        .find((sender) => sender.track.kind === videoTrack.kind)
-        .replaceTrack(videoTrack);
-
-      videoTrack.onended = function () {
-        const screenTrack = stream.getVideoTracks()[0];
-        myPeerConnection
-          .getSenders()
-          .find((sender) => sender.track.kind === screenTrack.kind)
-          .replaceTrack(screenTrack);
-        stream.getTracks().forEach((track) => track.stop());
-        share_screen.srcObject = myStream; // 본래 캠 들고오기
-        myPeerConnection
-          .getSenders()
-          .find((sender) => sender.track.kind === videoTrack.kind)
-          .replaceTrack(myStream.getVideoTracks()[0]);
-      };
-    });
-};
-
-const remoteCall = document.getElementById("remote");
-remoteCall.addEventListener("click", (e) => {
-  startCapture();
-});
